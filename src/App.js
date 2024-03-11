9b2aa39a13d923135dfb0051cdadcaddc9b74b78
Aleksey Beketov <alex311271@gmail.com>
2024-03-11 21:04:12 +0300

    set up a project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-address-book"></i>
+			<div>123</div>
+		</Div>
+	);
+};
